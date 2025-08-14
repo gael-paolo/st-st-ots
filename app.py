bf38bf44705b9aba53d4bf6738f016faf84563e6
gael-paolo <gael.velasquez.revilla@gmail.com>
2025-08-14 15:29:55 -0400

    Fix Secrets 2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from datetime import datetime
 # Leer variables desde st.secrets
 GMAIL_USER = st.secrets["email"]["GMAIL_USER"]
 GMAIL_PASSWORD = st.secrets["email"]["GMAIL_PASSWORD"]
-COPY_MAIL = ast.literal_eval(st.secrets["email"]["COPY_MAIL"])
+COPY_MAIL = st.secrets["email"]["COPY_MAIL"]
 GCS_BUCKET = st.secrets["gcp_config"]["GCS_BUCKET"]
 
 # Configurar credenciales GCP
